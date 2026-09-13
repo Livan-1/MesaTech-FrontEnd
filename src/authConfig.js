5818cbbc6c8022ab9092f38073b14e96611118a2
pauloberrios44 <166758722+pauloberrios44@users.noreply.github.com>
2026-09-13 12:53:40 -0300

    implementa axios y seguridad

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,9 +1,9 @@
 export const msalConfig = {
     auth: {
-        clientId: "499f7779-5088-4a08-b19d-2a23edcf2640",
+        clientId: "86c59355-8f99-4081-96c9-39db6e13ca08",
 
         authority:
-            "https://login.microsoftonline.com/828ad09d-e51b-4a6a-92e3-6e0737a5c395",
+            "https://login.microsoftonline.com/ba9c3522-adab-4c57-ac78-dc6131594e07",
 
         redirectUri: "http://localhost:3000/"
     },
@@ -15,4 +15,10 @@ export const msalConfig = {
 
 export const loginRequest = {
     scopes: ["openid", "profile", "email"]
+};
+
+export const apiRequest = {
+    scopes: [
+        "api://c639b2a3-10c3-4611-b3c8-8acb71c2afc6/access_as_user"
+    ]
 };
